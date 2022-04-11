6e0fc0e9285b0f1273eb60ea2caedb167cf459be
Alperen Yılmaz <theurgeboat@gmail.com>
2022-04-11 13:49:12 +0300

    kdv hesaplayici eklendi

---
+++ kadeve.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class kadeve
+{
+    public static void main(String[] args)
+    {
+        Scanner tara = new Scanner(System.in);
+
+
+        System.out.printf("Tutar giriniz (Kusuratli girebilirsiniz.):");
+        double tutar = tara.nextInt();
+
+        double KDVoran = (tutar<1000) ? 0.18:0.08;
+
+        System.out.printf("\nKDV orani = %.2f\nKDV'li fiyat = %.2f" ,KDVoran,(tutar*(1+KDVoran)));
+    }
+}
